3f207a7c1865e230a716600eacb41eba99c21dcf
TurtlePB <BreitensteinT@outlook.de>
2023-03-13 18:22:51 +0100

    heyho

--- Assets/Scripts/UIItem.cs
+++ Assets/Scripts/UIItem.cs
@@ -1,18 +1,22 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class UIItem : MonoBehaviour
+public class UIItem : MonoBehaviour, IPointerClickHandler
 {
     public Item item;
     private Image spriteImage;
+    private UIItem selectedItem;
 
     private void Awake()
     {
         spriteImage = GetComponent<Image>();
         UpdateItem(null);
+        selectedItem = GameObject.Find("SelectedItem").GetComponent<UIItem>();
     }
 
     public void UpdateItem(Item item)
@@ -26,6 +30,29 @@ public class UIItem : MonoBehaviour
         else
         {
             spriteImage.color = Color.clear;
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (this.item != null)
+        {
+            if (selectedItem != null)
+            {
+                Item clone = new Item(selectedItem.item);
+                selectedItem.UpdateItem(this.item);
+                UpdateItem(clone);
+            }
+            else
+            {
+                selectedItem.UpdateItem(this.item);
+                UpdateItem(null);
+            }
+        }
+        else if (selectedItem.item != null)
+        {
+            UpdateItem(selectedItem.item);
+            selectedItem.UpdateItem(null);
         }
     }
 }
