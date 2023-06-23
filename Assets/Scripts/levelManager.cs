6d9a923a8e16961f5d179d1828e7b47e6ed75dd6
TurtlePB <BreitensteinT@outlook.de>
2023-06-23 11:45:32 +0200

    jup

--- Assets/Scripts/levelManager.cs
+++ Assets/Scripts/levelManager.cs
@@ -1,12 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class levelManager : MonoBehaviour, IPointerDownHandler
 { 
-        public string sceneName;
+        [SerializeField] private string sceneName;
+        [SerializeField] private string menuName;
         private float clickCoolDown;
         
         void Awake()
@@ -14,9 +17,16 @@ public class levelManager : MonoBehaviour, IPointerDownHandler
             
             clickCoolDown = 5;
         }
-    
-        
-    
+
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                SceneManager.LoadScene(menuName);
+            }
+        }
+
+
         public void OnPointerDown(PointerEventData eventData)
         {
             SceneManager.LoadScene(sceneName);
